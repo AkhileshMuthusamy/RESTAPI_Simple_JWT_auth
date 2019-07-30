be48eb8e26f486882ac90ccf58325987feb69e6f
Akhilesh Muthusamy <AkhileshM@outlook.in>
2019-07-30 13:06:09 +0530

    Added morgan for logging

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const logger = require('morgan');
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
+app.use(logger('tiny'));
+
+const { port, appUrl } = require('./config');
+
+app.get('/', (req, res) => {
+    res.send('Server is up and running!');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on ${appUrl}`);
 });
