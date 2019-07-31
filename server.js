da484b1377fd961b650980a7906ae05e2966f9c9
Akhilesh Muthusamy <AkhileshM@outlook.in>
2019-07-31 23:11:01 +0530

    Added Auth route and instantiated mongo db connection

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
-
+const numCPUs = require('os').cpus().length;
+console.log('CPUs:' + numCPUs);
+const mongoose = require('mongoose');
 const logger = require('morgan');
+const { port, appUrl, databaseUrl } = require('./config');
 
-app.use(logger('tiny'));
+mongoose.connect(databaseUrl, { useNewUrlParser: true }, () => {
+    console.log('Connected to db!');
+});
 
-const { port, appUrl } = require('./config');
+//Import Routes
+const authRoute = require('./routes/auth');
+
+app.use(logger('dev'));
+//Route Middlewares
+app.use('/api/user', authRoute);
+
 
 app.get('/', (req, res) => {
     res.send('Server is up and running!');
