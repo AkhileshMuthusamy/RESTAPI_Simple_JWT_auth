2e7f20e9e8c06bbba00b84697505c95fe06c5e5b
Akhilesh Muthusamy <AkhileshM@outlook.in>
2019-07-31 23:36:19 +0530

    Added body parser middleware

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const app = express();
 const numCPUs = require('os').cpus().length;
 console.log('CPUs:' + numCPUs);
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const { port, appUrl, databaseUrl } = require('./config');
+//Import Routes
+const authRoute = require('./routes/auth');
 
 mongoose.connect(databaseUrl, { useNewUrlParser: true }, () => {
     console.log('Connected to db!');
 });
 
-//Import Routes
-const authRoute = require('./routes/auth');
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 //Route Middlewares
 app.use('/api/user', authRoute);
 
