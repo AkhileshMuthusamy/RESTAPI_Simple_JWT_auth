331badbb93056ab3eb2f5311c291170f4d074322
Akhilesh Muthusamy <AkhileshM@outlook.in>
2019-07-31 23:37:07 +0530

    Added save user to db logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const userModel = require('../model/user');
 
-router.post('/register', (req, res) => {
-    res.send('Register');
-})
+router.post('/register', async (req, res) => {
+    const user = new userModel({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const saveduser = await user.save();
+        res.send(saveduser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
 
 module.exports = router;
