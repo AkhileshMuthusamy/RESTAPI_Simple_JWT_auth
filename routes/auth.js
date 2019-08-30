366345c76b3a34d3bdcc70cefc8bc658f3fbe1b1
Akhilesh <AkhileshM@outlook.in>
2019-08-30 16:11:26 +0530

    updated  /login route to work asynchronously

--- routes/auth.js
+++ routes/auth.js
@@ -31,43 +31,47 @@ router.post('/register', async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', (req, res) => {
   // LETS VALIDATE THE DATA BEFORE CREATING A USER
   const { error } = loginValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   //Check if email already exists in the database
-  const user = await userModel.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("Email doesn't exists");
-
-  console.dir(user._id);
-  // Check if password is valid
-  bcrypt.compare(req.body.password, user.password).then(isMatch => {
-    console.log(isMatch);
-    if (isMatch) {
-      console.log('Password matched');
-      /**
-       * Create and assign a token
-       * expiresIn: seconds
-       */
-      jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, { expiresIn: 1 }, (error, token) => {
-        if (error) {
-          console.error(error.message);
-          return res.status(500).json({
-            error: 'Error signing token',
-            raw: error.message
-          });
-        }
-
-        return res.status(200).json({
-          success: true,
-          token: `Bearer ${token}`
-        });
-      });
-    } else {
-      console.log('Password mismatch');
-      return res.status(400).json({ error: 'Invalid password' });
+  userModel.findOne({ email: req.body.email }).then(user => {
+    if (!user) {
+      return res.status(400).json({ error: "Email doesn't exists" });
     }
+
+    console.dir(user._id);
+
+    // Check if password is valid
+    bcrypt.compare(req.body.password, user.password).then(isMatch => {
+      console.log(isMatch);
+      if (isMatch) {
+        console.log('Password matched');
+        /**
+         * Create and assign a token
+         * expiresIn: seconds
+         */
+        jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, { expiresIn: 1 }, (error, token) => {
+          if (error) {
+            console.error(error.message);
+            return res.status(500).json({
+              error: 'Error signing token',
+              raw: error.message
+            });
+          }
+
+          return res.status(200).json({
+            success: true,
+            token: `Bearer ${token}`
+          });
+        });
+      } else {
+        console.log('Password mismatch');
+        return res.status(400).json({ error: 'Invalid password' });
+      }
+    });
   });
 });
 
